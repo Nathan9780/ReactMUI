9e929a4621b837795c5469e237a6c3aa03116460
Nathan e Renan <0001151944@senaimgaluno.com.br>
2026-02-24 17:36:28 -0300

    V2

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import FormaCadastro from "./components/FormCadastro";
 import Quantidade from "./components/Quantidade";
 import FormPreferencias from "./components/Preferencias";
 import FormConfiguracoes from "./components/Config";
+import CadastrodeProdutos from "./components/CadastroProduct";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -12,5 +13,6 @@ createRoot(document.getElementById("root")).render(
     <Quantidade />
     <FormPreferencias />
     <FormConfiguracoes />
+    <CadastrodeProdutos />
   </StrictMode>,
 );
